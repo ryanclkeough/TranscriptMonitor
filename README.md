cb2a72742594c550ce769748ae04c621c64d0fb3
Ryan <ryankeough0311@users.noreply.github.com>
2024-10-11 21:40:14 -0500

    Added handling for repeat blacklist terms, fixing certain bugs, reworking Readme

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # TranscriptMonitor
 Takes in a text file of a video transcript, and parses the document for any profanity. This will return either a pass, or a list of bad words paired with their time stamps
+
+## Steps
+1. Add 1 or more .txt transcript files to the `Transcripts` folder (Ensure your transcript has timestamps)
+2. Create a json with your list of blacklisted terms with the filename `BlacklistedTerms.json`. The current repo does not have a blacklist file yet, so you will need to make your own.
+3. Run!
+4. Remove added transcript files after use
+
+## Disclaimer
+Do not add anything to the downloaded repo except .txt trascript files in the `Transcripts` folder. 
+Do not delete anything from the repo except the .txt trascript files you added in the `Transcripts` folder.
+If for any reason you did not follow these steps, the script might not work as expected
--- TranscriptMonitor.py
+++ TranscriptMonitor.py
@@ -8,7 +8,8 @@ def main():
     transcriptContents = GetTranscriptFolderContents()
     if transcriptContents:
         blacklistedTerms = GetBlacklistedTerms()
-        ParseTranscripts(transcriptContents, blacklistedTerms)
+        errorMessages = ParseTranscripts(transcriptContents, blacklistedTerms)
+        PrintResults(errorMessages)
 
 def GetTranscriptFolderContents():
     transcriptContents = {}
@@ -21,9 +22,9 @@ def GetTranscriptFolderContents():
     else:
         for item in os.listdir(transcriptsDirectory):
             if not os.path.isfile(os.path.join(transcriptsDirectory, item)):
-                print(f"Item {item} is not a file and cannot be processed")
+                print(f"The item \"{item}\" in \"{transcriptsDirectory}\" is not a file and cannot be processed")
             elif not item.endswith(".txt"):
-                print(f"File {item} is not of type .txt and cannot be processed.")
+                print(f"The file \"{item}\" is not of type .txt and cannot be processed.")
             else:
                 textFileContents = {}
                 textFileName = os.path.basename(item)
@@ -43,6 +44,10 @@ def GetTranscriptFolderContents():
                         else:
                             timestampText += line + " "
 
+                    # Grabs final timestamp/text pair
+                    if timestamp != "":
+                        textFileContents[timestamp] = timestampText
+                        
                 transcriptContents[textFileName] = textFileContents
 
     return transcriptContents
@@ -60,11 +65,40 @@ def GetBlacklistedTerms():
     return blacklistedTerms
 
 def ParseTranscripts(transcriptContents, blacklistedTerms):
+    errorMessages = []
     for file in transcriptContents:
         for timeStamp in transcriptContents[file]:
+            foundBlacklistedTerms = []
             for blacklistedTerm in blacklistedTerms:
                 if blacklistedTerm in transcriptContents[file][timeStamp]:
-                    print(f"The blacklisted term \"{blacklistedTerm}\" exists in the file \"{file}\" at time stamp {timeStamp}")
+                    foundBlacklistedTerms = AddBlacklistedTermToList(blacklistedTerm, foundBlacklistedTerms)
+                    
+            if len(foundBlacklistedTerms) > 0:
+                errorMessages.append(f"The blacklisted term(s) {foundBlacklistedTerms} exists in the file \"{file}\" at time stamp {timeStamp}")
+
+    return errorMessages
+
+def AddBlacklistedTermToList(newBlacklistedTerm, foundBlacklistedTerms):
+    needToUpdateList = True
+    for index, foundBlacklistedTerm in enumerate(foundBlacklistedTerms):
+        if newBlacklistedTerm in foundBlacklistedTerm:
+            needToUpdateList = False
+            break
+        elif foundBlacklistedTerm in newBlacklistedTerm:
+            foundBlacklistedTerms[index] = newBlacklistedTerm
+            needToUpdateList = False
+
+    if needToUpdateList:
+        foundBlacklistedTerms.append(newBlacklistedTerm)
+    
+    return foundBlacklistedTerms
+
+def PrintResults(errorMessages):
+    if len(errorMessages) == 0:
+        print("There were no blacklisted terms found within any of your transcripts")
+    else:
+        for message in errorMessages:
+            print(message)
 
 if __name__ == '__main__':
     main()
